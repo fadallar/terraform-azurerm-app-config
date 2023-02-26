001391e68d25d232e0c45027470526bb6cf546c8
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-26 12:26:35 +0100

    Adding Identity support

--- identities.tf
+++ identities.tf
@@ -1,28 +1,28 @@
-// ACR PULL
-resource "azurerm_user_assigned_identity" "acr_pull" {
+// APP Conf Data Reader
+resource "azurerm_user_assigned_identity" "apconf_data_reader" {
   location            = var.location
-  name                = format("id-acrpull-%s", azurerm_container_registry.registry.name)
+  name                = format("id-data-reader-%s", azurerm_app_configuration.appconf.name)
   resource_group_name = var.resource_group_name
   tags                = var.default_tags
 }
 
-resource "azurerm_role_assignment" "rbac_acr_pull" {
-  scope                = azurerm_container_registry.registry.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_user_assigned_identity.acr_pull.principal_id
+resource "azurerm_role_assignment" "rbac_apconf_data_reader" {
+  scope                = azurerm_app_configuration.appconf.id
+  role_definition_name = "App Configuration Data Reader"
+  principal_id         = azurerm_user_assigned_identity.apconf_data_reader.principal_id
 }
 
-// ACR PUSH
+// APP Conf Data Owner
 
-resource "azurerm_user_assigned_identity" "acr_push" {
+resource "azurerm_user_assigned_identity" "apconf_data_owner" {
   location            = var.location
-  name                = format("id-acrpush-%s", azurerm_container_registry.registry.name)
+  name                = format("id-data-owner-%s", azurerm_app_configuration.appconf.name)
   resource_group_name = var.resource_group_name
   tags                = var.default_tags
 }
 
-resource "azurerm_role_assignment" "rbac_acr_push" {
-  scope                = azurerm_container_registry.registry.id
-  role_definition_name = "AcrPush"
-  principal_id         = azurerm_user_assigned_identity.acr_push.principal_id
+resource "azurerm_role_assignment" "rbac_appconf_data_owner" {
+  scope                = azurerm_app_configuration.appconf.id
+  role_definition_name = "App Configuration Data Owner"
+  principal_id         = azurerm_user_assigned_identity.apconf_data_owner.principal_id
 }
